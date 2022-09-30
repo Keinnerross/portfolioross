8ccb1c2f98e0e7340eb088e129980cfed28a894f
keinnerross <keinnerross@gmail.com>
2022-09-29 21:44:09 -0300

    update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,16 +6,37 @@ const About = () => {
     <div id="about" className="about-container">
       <div className="about-content-container">
         <div className="techsopher-container">
-            <Techsopher></Techsopher>
+          <Techsopher></Techsopher>
         </div>
         <div className="about-info">
-          <h4>About</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-            velit nulla corporis ab illo at dicta, consectetur eaque obcaecati
-            quod libero doloremque quae quidem saepe. Optio sequi libero
-            eligendi a? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt excepturi id magni laboriosam quam ea tempore, sint illo facere perferendis doloremque. Quis libero laudantium provident dolorum beatae similique voluptatem quia? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quas facere officiis incidunt odio nulla ullam? Accusantium in, magnam nisi praesentium ipsa totam quisquam, non, ullam nesciunt cumque iste inventore.
-          </p>
+          <section className="info">
+            <h4>About</h4>
+            <p>
+              Me llamo KeinnerRoss ( nacido en 1998 - Venezuela)
+              <span className="purple-1">
+                soy diseñador y desarrollador Frontend,
+              </span>
+              he trabajado como freelancer en estos últimos años y ahora busco
+              nuevos retos donde pueda fortalecer mis habilidades y trabajar en
+              equipo. Amo la naturaleza, la historia y las arepas.
+            </p>
+          </section>
+          <section className="interest-info">
+          <h2>Intereses</h2>
+          <div className="interests">
+            <div className="interest">
+              <div className="interest-ico procedural"></div>Procedural
+              programming
+            </div>
+            <div className=" interest">
+              <div className="interest-ico platzi"></div>Comunidad Platzi
+            </div>
+            <div className="interest">
+              <div className="interest-ico filosofia"></div>Filosofía de la
+              tecnología
+            </div>
+          </div>
+          </section>
         </div>
       </div>
     </div>
