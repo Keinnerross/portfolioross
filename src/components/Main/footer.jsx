746326f7c3c9054db15aaf421de953b0823d532b
KeinnerRoss <keinnerross@gmail.com>
2023-08-03 17:40:14 -0400

    footerChange

--- src/components/Main/footer.jsx
+++ src/components/Main/footer.jsx
@@ -3,9 +3,21 @@ import "../../stylesheet/main/footer.css";
 const Footer = () => {
   return (
     <footer>
-      <span className="footer-text">
+      <div className="footerSection">
+
+        <div className="infoFooter">
+          <span>KeinnerRoss</span>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicin.</p>
+          <span>Escribeme un whatsapp</span>
+          <h4>9 9130 2647</h4>
+        </div>
+        <div className="menufooterSection">
+          <div className="footerColumn"><h5>Plataforma</h5> <ul><li>Trabajos</li><li>Sobre Mi</li></ul></div>
+        </div>
+      </div>
+      {/* <span className="footer-text">
         Hecho con ♥ por <span>Keinner Ross</span> ©2023
-      </span>
+      </span> */}
     </footer>
   );
 };
